c0e62e2c48c1517831d3ce35112ae9c2161fa806
Robert <rperron91@gmail.com>
2019-05-15 17:16:54 -0400

    Chart Displays Information
    Chart shows the very basic data and is still not formatted in a way that is easy to read or intuitive to work with. Need to add other datasets, labels to markers, and fix tooltips.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,17 @@
 // @TODO: YOUR CODE HERE!
 // Define SVG area dimensions
+function makeResponsive() {
+
+  // if the SVG area isn't empty when the browser loads,
+  // remove it and replace it with a resized version of the chart
+  var svgArea = d3.select("body").select("svg");
+
+  // clear svg if not empty
+  if (!svgArea.empty()) {
+    svgArea.remove();
+  }
+  
+
 var svgWidth = 960;
 var svgHeight = 660;
 
@@ -29,32 +41,31 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${chartMargin.left*2}, ${chartMargin.bottom*2})`);
 
 // Load Data from csv and use values for scatter plot points and axes  
-d3.csv("../assets/data/data.csv", function(data) {
+d3.csv("../assets/data/data.csv").then(function(data) {
   
   // Make sure categories being used are all numeric
-  for(i =1; i < data.length; i++) {
-    data[i].poverty = +data[i].poverty
-    data[i].age = +data[i].age
-    data[i].income = +data[i].income
-    data[i].healthcare = +data[i].healthcare
-    data[i].obesity = +data[i].obesity
-    data[i].smokes = +data[i].smokes
-
-  };
+  data.forEach(function(data) { 
+    data.poverty = +data.poverty;
+    data.age = +data.age;
+    data.income = +data.income;
+    data.healthcare = +data.healthcare;
+    data.obesity = +data.obesity;
+    data.smokes = +data.smokes;
+  });
   // Console log the data to check if everything is formatted correctly 
   console.log(data);
 
 
   // scale y to chart height using healthcare data for ticks
   var yScale = d3.scaleLinear()
-    .domain([0, d3.max(data.healthcare)])
+    .domain([0, d3.max(data, d => d.healthcare)])
     .range([chartHeight, 0]);
 
 
   // scale x to chart width using poverty data for ticks
   var xScale = d3.scaleLinear()
-      .domain(d3.min(data.poverty), d3.max(data.poverty))
-      .range([0, chartWidth]);
+      .domain([d3.min(data, d => d.poverty), d3.max(data, d => d.poverty)])
+      .range([chartMargin.left, chartWidth-chartMargin.left]);
       
   // Set scales to corresponding Axes
   var yAxis = d3.axisLeft(yScale).ticks(10);
@@ -62,12 +73,12 @@ d3.csv("../assets/data/data.csv", function(data) {
   
   // set x to the x axis
   chartGroup.append("g")
-    .attr("transform", `translate(0, ${chartHeight-60})`)
+    .attr("transform", `translate(0, ${chartHeight-2*chartMargin.left-5})`)
     .call(xAxis);
 
   // set y to the y axis
   chartGroup.append("g")
-    .attr("transform", `translate(0, ${-60})`)
+    .attr("transform", `translate(${chartMargin.left}, ${-2*chartMargin.top-5})`)
     .call(yAxis);
 
 
@@ -133,48 +144,71 @@ d3.csv("../assets/data/data.csv", function(data) {
       .data(data)
       .enter()
       .append("circle")
-      .attr("cy", yScale(d => d.healthcare))
-      .attr("cx", xScale(d => d.poverty))
-      .attr("r", 10)
-      .attr("id", data.state)
-      .attr("fill", "blue");
+      .attr("cy", d=> yScale(d.healthcare))
+      .attr("cx", d=> xScale(d.poverty))
+      .attr("r", "8")
+      .classed("stateCircle", true);
 
-  // append circles to group
-  circles.append("circle")
+    // var circleText = chartGroup.select("circle")
+    //   .data(data.abbr)
+    //   .append("text")
+    //   .attr("y", d=> yScale(d.healthcare))
+    //   .attr("x", d=> xScale(d.poverty))
+    //   .classed("stateText", true);
+
+  var toolTip = d3.select("body")
+    .append("div")
+    .classed("d3-tip", true);
+
+  //append circles with mouseover event
+  circles.on("mouseover", d => {
+    console.log(d.abbr)
+    toolTip.style("display", "block")
+      .html(
+      `<strong>${d.state}<strong><hr>${d.poverty}
+  percent of population in poverty<hr>${d.healthcare}
+  percent of population without healthcare`)
+      .style("left", d3.event.pageX + "px")
+      .style("top", d3.event.pageY + "px");
+  })
 
   // append title(s) to bottom center of chart
   chartGroup.append("text")
     .attr("transform", `translate(${chartWidth/2}, ${chartHeight-35})`)
-    .classed("poverty-text", true)
-    .text("poverty");
+    .classed("aText", true)
+    .text("% in Poverty");
 
   // chartGroup.append("text")
   //   .attr("transform", `translate(${chartWidth/2}, ${chartHeight-20})`)
-  //   .classed("age-text", true)
-  //   .text("age");
+  //   .classed("aText", true)
+  //   .text("Age");
 
   // chartGroup.append("text")
   //   .attr("transform", `translate(${chartWidth/2}, ${chartHeight-5})`)
-  //   .classed("income-text", true)
-  //   .text("income");
+  //   .classed("aText", true)
+  //   .text("Income");
     
   // rotate title(s) and append to left center of chart
   chartGroup.append("text")
-    .attr("transform", `translate(${-chartMargin.left}, ${chartHeight/2})rotate(${-90})`)
-    .classed("healthcare-text", true)
-    .text("healthcare");
+    .attr("transform", `translate(${-0}, ${(chartHeight-2*chartMargin.top)/2})rotate(${-90})`)
+    .classed("aText", true)
+    .text("% Lacking Healthcare");
 
   // chartGroup.append("text")
-  //   .attr("transform", `translate(${-2*chartMargin.left}, ${chartHeight/2})rotate(${-90})`)
-  //   .classed("obesity-text", true)
-  //   .text("obesity");
+  //   .attr("transform", `translate(${-20}, ${(chartHeight-2*chartMargin.top)/2})rotate(${-90})`)
+  //   .classed("aText", true)
+  //   .text("% Pop. Obese");
 
   // chartGroup.append("text")
-  //   .attr("transform", `translate(${-3*chartMargin.left}, ${chartHeight/2})rotate(${-90})`)
-  //   .classed("smokes-text", true)
-  //   .text("smokes");
+  //   .attr("transform", `translate(${-40}, ${(chartHeight-2*chartMargin.top)/2})rotate(${-90})`)
+  //   .classed("aText", true)
+  //   .text("% Smokes Regularly");
 });
+};
 
+makeResponsive();
+
+d3.select(window).on("resize", makeResponsive());
 
 
 
